92023a7a0947756723bb3c3a08c6d572abf65515
zeraX23 <officialjiyora@gmail.com>
2026-06-25 03:09:21 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
 const CACHE_NAME = 'respek-pwa-v1'; // Perbaikan typo: 'Const' menjadi 'const'
 
 // Daftar file yang akan disimpan di memori lokal HP/Browser
